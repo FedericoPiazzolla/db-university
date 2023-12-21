dace2e4de4da7c3df98882e1d6e553908f0c38d4
FedericoPiazzolla <Federicop257@gmail.com>
2023-12-21 18:05:21 +0100

    wip: bonus

--- university-db-join.sql
+++ university-db-join.sql
@@ -55,3 +55,9 @@ ON `departments`.`id` = `degrees`.`department_id`
 WHERE `departments`.`name` = "Dipartimento di Matematica"
 
 -- 7 BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT * 
+FROM `students`
+JOIN `exam_student`
+ON `students`.`id` = `exam_student`.`student_id`
+JOIN `exams`
+ON `exams`.`id` = `exam_student`.`exam_id`

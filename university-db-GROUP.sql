994b0c8307a44c728aebb49fee4cbfe9c6a55be8
FedericoPiazzolla <Federicop257@gmail.com>
2023-12-22 10:07:17 +0100

    add: method for filter and round

--- university-db-GROUP.sql
+++ university-db-GROUP.sql
@@ -2,6 +2,9 @@
 SELECT YEAR(`enrolment_date`), COUNT(*) AS `num_students`
 FROM `students`
 GROUP BY YEAR(`enrolment_date`);
+-- lo utiliziamo per filtrare i risultati della nuova tabella creata con group 
+HAVING `num_students` > 1000 --> filtrerà gli studenti che sono maggiori di 1000
+
 
 -- 2 Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 SELECT `office_address`, COUNT(*) as `tot_teacher`
@@ -9,7 +12,7 @@ FROM `teachers`
 GROUP BY `office_address`
 
 -- 3 Calcolare la media di ogni appello d'esame
-SELECT `exam_id`, AVG(`exam_student`.`vote`) AS `avg_vote`
+SELECT `exam_id`, ROUND(AVG(`exam_student`.`vote`), 2) AS `avg_vote` --> con ROUND approssimiamo a 2 cifre dopo la virgola con , 2
 FROM `exam_student`
 GROUP BY `exam_id`
 
